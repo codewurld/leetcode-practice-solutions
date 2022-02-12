594b10001f3e5409c5fd01d065588bc7e163962f
codewurld <sholaq@gmail.com>
2022-02-12 15:15:27 +0000

    solution 1512

---
+++ Arrays/numIdenticalPairs.js
@@ -0,0 +1,15 @@
+var numIdenticalPairs = function (nums) {
+
+    let goodPairsCount = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+
+            if (nums[i] == nums[j]) {
+                goodPairsCount++;
+            }
+        }
+
+    }
+    return goodPairsCount;
+};
