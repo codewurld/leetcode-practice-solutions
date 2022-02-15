ebfce8eeb1f884e939ddd823c1d9994fa30d6f30
codewurld <sholaq@gmail.com>
2022-02-15 15:11:50 +0000

    binary search solution to problem 852

--- Arrays/peakIndex.js
+++ Arrays/peakIndex.js
@@ -6,9 +6,30 @@
 var peakIndexInMountainArray = function (arr) {
 
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] > arr[i + 1]) {
-            return 1;
+        if (arr[i] >= arr[i + 1] && arr[i] >= arr[i - 1]) {
+            return i;
         }
     }
-
+    return -1;
 };
+
+// Binary Search
+
+var peakIndexInMountainArray = function (arr) {
+
+    let startIndex = 0;
+    let endIndex = arr.length - 1;
+
+    while (startIndex < endIndex) {
+        let midIndex = startIndex + Math.floor((endIndex - startIndex) / 2);
+
+        if (arr[midIndex] < arr[midIndex + 1]) {
+
+            startIndex = midIndex + 1;
+        } else {
+            endIndex = midIndex;
+        }
+
+    }
+    return startIndex;
+}
