ef2b855e2e41fbfd42067357443273f978cf9b12
codewurld <sholaq@gmail.com>
2022-02-21 16:27:46 +0000

    undirected graph algorithm

---
+++ Arrays/algorithms/undirectGraph.js
@@ -0,0 +1,42 @@
+const edges = [
+    ['i', 'j'],
+    ['k', 'i'],
+    ['m', 'k'],
+    ['k', 'l'],
+    ['o', 'n']
+]
+
+// converts AM -> AL
+const buildGraph = (edges) => {
+    const graph = {};
+
+    for (let edge of edges) {
+        const [a, b] = edge;
+        if (!(a in graph)) graph[a] = [];
+        if (!(b in graph)) graph[b] = [];
+        graph[a].push(b);
+        graph[b].push(a);
+    }
+    return graph;
+}
+
+const undirectedPath = (edges, nodeA, nodeB) => {
+    const graph = buildGraph(edges);
+
+    return hasPath(graph, nodeA, nodeB, new Set());
+}
+
+const hasPath = (graph, src, dest, visited) => {
+    if (src === dest) return true;
+    if (visited.has(src)) return false;
+
+    visited.add(src);
+
+    for (let neighbour of graph[src]) {
+        if (hasPath(graph, neighbour, dest, visited)) {
+            return true;
+        }
+    }
+    return false;
+
+}
