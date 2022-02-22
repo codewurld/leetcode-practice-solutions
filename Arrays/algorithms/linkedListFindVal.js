f78d6733e2c5a1b35bd7f368090be9d7b288ff17
codewurld <sholaq@gmail.com>
2022-02-22 14:30:53 +0000

    find value in linkedList

---
+++ Arrays/algorithms/linkedListFindVal.js
@@ -0,0 +1,27 @@
+class NodeNo {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+const a = new NodeNo(2);
+const b = new NodeNo(8);
+const c = new NodeNo(3);
+const d = new NodeNo(7);
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+const findVal = (head, target) => {
+    let currentPointer = head;
+
+    while (currentPointer !== null) {
+        if (currentPointer.val === target) return true;
+        currentPointer = currentPointer.next;
+    }
+    return false;
+}
+
+console.log(findVal(a, 3));
