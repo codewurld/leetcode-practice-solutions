384c11895d87fa88c6fe3cbd651c07ec7da8b220
codewurld <sholaq@gmail.com>
2022-02-23 16:19:19 +0000

    longest common prefix solution

---
+++ Arrays/longestCommonPrefix.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    let longest = "";
+
+    if (strs.length === 0) return longest;
+
+    let comparisonWord = strs[0];
+    let comparisonIndex = 0;
+
+    for (let comparisonLetter of comparisonWord) {
+        for (let i = 1; i < strs.length; i++) {
+            let currentWord = strs[i];
+            let currentLetter = currentWord.charAt(comparisonIndex);
+
+            if (comparisonLetter !== currentLetter || comparisonIndex > currentWord.length) {
+                return longest;
+            }
+
+        }
+        longest += comparisonLetter;
+        comparisonIndex++;
+    }
+    return longest;
+};
