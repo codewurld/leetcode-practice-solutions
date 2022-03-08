a142140190b559637a34bb7751ef00463aba635d
codewurld <sholaq@gmail.com>
2022-03-08 13:04:03 +0000

    sum binary tree traversal

--- Arrays/algorithms/binarytree.js
+++ Arrays/algorithms/binarytree.js
@@ -138,3 +138,44 @@ const treeIncludes = (root, target) => {
 }
 
 console.log(treeIncludes(a, 'j'));
+
+// sum Traversal - iterative
+
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+const sumTraversal = (root) => {
+
+    const stack = [root];
+    let sum = 0;
+
+    while (stack.length > 0) {
+        const current = stack.pop();
+
+        sum += current.val;
+
+        if (current.left) stack.push(current.left);
+        if (current.right) stack.push(current.right);
+    }
+    return sum;
+}
+
+console.log(sumTraversal(a));
