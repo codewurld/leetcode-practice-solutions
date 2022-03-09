47447ba4200cfe2754956bd6249b63478cffa6cf
codewurld <sholaq@gmail.com>
2022-03-09 19:27:45 +0000

    solution to problem 26 removeDuplicates

---
+++ Arrays/removeDuplicates.js
@@ -0,0 +1,14 @@
+// problem 26 - remove duplicates
+
+var removeDuplicates = function (nums) {
+
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i + 1] === nums[i]) {
+            nums.splice(i, 1)
+            i--;
+        }
+    }
+
+    return nums.length;
+};
