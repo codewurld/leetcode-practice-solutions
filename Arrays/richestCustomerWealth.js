379b50c64ae7e74b53419a86690f64c96ba6a2f3
codewurld <sholaq@gmail.com>
2022-04-05 16:42:39 +0100

    richest customer wealth solution 1672

---
+++ Arrays/richestCustomerWealth.js
@@ -0,0 +1,28 @@
+// 1672. Richest Customer Wealth
+
+// You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+
+// A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+/**
+ * @param {number[][]} accounts
+ * @return {number}
+ */
+var maximumWealth = function (accounts) {
+
+    let maxWealth = 0;
+
+    for (let currentAccount of accounts) {
+        let currentSum = currentAccount.reduce((a, b) => a + b);
+        maxWealth = Math.max(maxWealth, currentSum);
+    }
+
+
+
+    return maxWealth;
+
+    // return Math.max(allSums);
+
+};
+
+  // console.log(maximumWealth([3,5,7]));
