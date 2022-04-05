fc0f55ea63c7f0977ec0bbcea385599d5d568f76
codewurld <sholaq@gmail.com>
2022-04-05 16:31:50 +0100

    identical pairs efficient solution

--- Arrays/noOfGoodPairs.js
+++ Arrays/noOfGoodPairs.js
@@ -1,5 +1,7 @@
 // Solution 1512 No of Good Pairs
 
+// O(N^2) O(1)
+
 var numIdenticalPairs = function (nums) {
 
     let goodPairsCount = 0;
@@ -14,4 +16,26 @@ var numIdenticalPairs = function (nums) {
 
     }
     return goodPairsCount;
+};
+
+// O(N) O(N)
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function (nums) {
+
+    if (nums.length === 0) return 0;
+
+    let map = {};
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (map[nums[i]] !== undefined) {
+            count += map[nums[i]]++;
+        } else {
+            map[nums[i]] = 1;
+        }
+    }
+    return count;
 };
