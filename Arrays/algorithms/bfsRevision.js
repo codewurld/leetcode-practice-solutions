eb8901894ca648a9b66532240f468007e4dd98a1
codewurld <sholaq@gmail.com>
2022-04-26 17:24:19 +0100

    bfs has path

--- Arrays/algorithms/bfsRevision.js
+++ Arrays/algorithms/bfsRevision.js
@@ -23,3 +23,25 @@ const bfs = (graph, sourceNode) => {
 }
 
 console.log(bfs(myGraph, 'c'))
+
+
+const bfsPath = (graph, sourceNode, dest) => {
+
+    if (sourceNode === dest) return true;
+
+    const queue = [sourceNode];
+
+    while (queue.length > 0) {
+        const currentNode = queue.shift();
+
+
+        for (let neighbour of graph[currentNode]) {
+            if (neighbour === dest) return true;
+
+            queue.push(neighbour);
+        }
+    }
+    return false;
+}
+
+console.log(bfs(myGraph, 'a', 'e'))
