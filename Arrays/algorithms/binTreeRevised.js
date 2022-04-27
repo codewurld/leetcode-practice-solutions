a4292380d061ef5d06a4d0bee32261960ef10493
codewurld <sholaq@gmail.com>
2022-04-27 15:43:16 +0100

    find target in binary tree

--- Arrays/algorithms/binTreeRevised.js
+++ Arrays/algorithms/binTreeRevised.js
@@ -38,3 +38,36 @@ const binaryTree = (root) => {
 }
 
 console.log(binaryTree(a));
+
+
+// find target 
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const a = new Node(5);
+const b = new Node(10);
+const c = new Node(15);
+const d = new Node(20);
+const e = new Node(25);
+const f = new Node(30);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+const binaryTreeTarget = (root, target) => {
+
+    if (root === null) return false;
+    if (root.val === target) return true;
+
+    return binaryTree(root.left, target) || binaryTree(root.right, target);
+}
+
+console.log(binaryTree(a, 70));
